052a7c7a6513187c258f65a28148c38566ed51f8
VJKUMAR <115359380+letsmailvjkumar@users.noreply.github.com>
2023-06-28 15:50:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+window.onload = function() {
+  var levelElement = document.getElementById('level');
+  var level = 1;
+
+  while (levelElement !== null) {
+    level++;
+    levelElement = levelElement.parentElement;
+  }
+
+  alert('The level of the element is: ' + level);
+};
